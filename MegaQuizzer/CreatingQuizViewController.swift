e09cca45d08d2900166892fe8dd2c15fe33cd3c0
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-31 00:29:50 +0300

    fixed GUI

---
+++ MegaQuizzer/CreatingQuizViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class CreatingQuizViewController: UIViewController {
+
+    @IBOutlet weak var quizNameTextField: UITextField!
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func nextTapped() {
+        
+    }
+    
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
